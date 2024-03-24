806c20d4d28fcdaba0d19db15633b73beee97f0b
Andrew <alrossell@gmail.com>
2024-03-24 09:19:29 -0700

    First commit

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "database/sql"
+
+    _ "github.com/lib/pq"
+)
+
+type Song struct {
+    Id int
+    Title string
+    Artist string
+    Album string
+    ReleaseYear int
+    Genre string
+    DurationSeconds int
+}
+
+
+func main() {
+    fmt.Println("Creating a new song")
+
+    connStr := "user=postgres password=mypasswpord dbname=new_database sslmode=disable"
+   
+    var err error
+    client, err := sql.Open("postgres", connStr)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    var songs []Song = []Song{
+        Song{Title: "A Title", Artist: "A Artist", Album: "A Album", ReleaseYear: 2020, Genre: "Pop", DurationSeconds: 180},
+        Song{Title: "B Title", Artist: "B Artist", Album: "B Album", ReleaseYear: 2021, Genre: "Rock", DurationSeconds: 200},
+        Song{Title: "C Title", Artist: "C Artist", Album: "C Album", ReleaseYear: 2022, Genre: "Jazz", DurationSeconds: 220},
+        Song{Title: "D Title", Artist: "D Artist", Album: "D Album", ReleaseYear: 2020, Genre: "Pop", DurationSeconds: 180},
+        Song{Title: "E Title", Artist: "E Artist", Album: "E Album", ReleaseYear: 2020, Genre: "Pop", DurationSeconds: 180},
+        Song{Title: "F Title", Artist: "F Artist", Album: "F Album", ReleaseYear: 2021, Genre: "Rock", DurationSeconds: 200},
+        Song{Title: "G Title", Artist: "G Artist", Album: "G Album", ReleaseYear: 2022, Genre: "Jazz", DurationSeconds: 220},
+        Song{Title: "H Title", Artist: "H Artist", Album: "H Album", ReleaseYear: 2021, Genre: "Rock", DurationSeconds: 200},
+        Song{Title: "I Title", Artist: "I Artist", Album: "I Album", ReleaseYear: 2022, Genre: "Jazz", DurationSeconds: 220},
+    }
+
+    for _, song := range songs {
+        _, err = client.Exec(
+            `INSERT INTO songs (title, artist, album, release_year, genre, duration_seconds)
+             VALUES ($1, $2, $3, $4, $5, $6)`,
+            song.Title, song.Artist, song.Album, song.ReleaseYear, song.Genre, song.DurationSeconds)
+
+        if err != nil {
+            log.Fatal(err)
+            fmt.Println("Error creating song")
+        }
+    }
+
+    fmt.Println("Song created successfully!")
+}
+
