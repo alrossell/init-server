0d4306251bb0e2254b0b6a8cd4e01a5b2e3978c4
Andrew <alrossell@gmail.com>
2024-03-27 19:01:04 -0700

    Modifying reviews table and adding documentation for the scheme of the table

--- main.go
+++ main.go
@@ -31,9 +31,9 @@ func main() {
     }
 
     createTableSQL := `CREATE TABLE IF NOT EXISTS songs (
-        id SERIAL PRIMARY KEY,
-        artist TEXT,
-        albun TEXT,
+        review_id SERIAL PRIMARY KEY,
+        artist TEXT NOT NULL,
+        albun TEXT NOT NULL,
         release_year INT,
         genre TEXT,
         duration_seconds INT
@@ -60,35 +60,50 @@ func main() {
         log.Println("Row count: ", numberOfRows)
     }
 
-    if numberOfRows > 0 {
-        log.Println("Table already has data")
-        return
-    }
-
-    var songs []Song = []Song{
-        Song{Title: "A Title", Artist: "A Artist", Album: "A Album", ReleaseYear: 2020, Genre: "Pop", DurationSeconds: 180},
-        Song{Title: "B Title", Artist: "B Artist", Album: "B Album", ReleaseYear: 2021, Genre: "Rock", DurationSeconds: 200},
-        Song{Title: "C Title", Artist: "C Artist", Album: "C Album", ReleaseYear: 2022, Genre: "Jazz", DurationSeconds: 220},
-        Song{Title: "D Title", Artist: "D Artist", Album: "D Album", ReleaseYear: 2020, Genre: "Pop", DurationSeconds: 180},
-        Song{Title: "E Title", Artist: "E Artist", Album: "E Album", ReleaseYear: 2020, Genre: "Pop", DurationSeconds: 180},
-        Song{Title: "F Title", Artist: "F Artist", Album: "F Album", ReleaseYear: 2021, Genre: "Rock", DurationSeconds: 200},
-        Song{Title: "G Title", Artist: "G Artist", Album: "G Album", ReleaseYear: 2022, Genre: "Jazz", DurationSeconds: 220},
-        Song{Title: "H Title", Artist: "H Artist", Album: "H Album", ReleaseYear: 2021, Genre: "Rock", DurationSeconds: 200},
-        Song{Title: "I Title", Artist: "I Artist", Album: "I Album", ReleaseYear: 2022, Genre: "Jazz", DurationSeconds: 220},
-    }
-
-    for _, song := range songs {
-        _, err = client.Exec(
-            `INSERT INTO songs (title, artist, album, release_year, genre, duration_seconds)
-             VALUES ($1, $2, $3, $4, $5, $6)`,
-            song.Title, song.Artist, song.Album, song.ReleaseYear, song.Genre, song.DurationSeconds)
-
-        if err != nil {
-            log.Fatal(err)
-            fmt.Println("Error creating song")
+    if numberOfRows == 0 {
+        log.Println("Inserting data into songs table.")
+        var songs []Song = []Song{
+            {Title: "A Title", Artist: "A Artist", Album: "A Album", ReleaseYear: 2020, Genre: "Pop", DurationSeconds: 180},
+            {Title: "B Title", Artist: "B Artist", Album: "B Album", ReleaseYear: 2021, Genre: "Rock", DurationSeconds: 200},
+            {Title: "C Title", Artist: "C Artist", Album: "C Album", ReleaseYear: 2022, Genre: "Jazz", DurationSeconds: 220},
+            {Title: "D Title", Artist: "D Artist", Album: "D Album", ReleaseYear: 2020, Genre: "Pop", DurationSeconds: 180},
+            {Title: "E Title", Artist: "E Artist", Album: "E Album", ReleaseYear: 2020, Genre: "Pop", DurationSeconds: 180},
+            {Title: "F Title", Artist: "F Artist", Album: "F Album", ReleaseYear: 2021, Genre: "Rock", DurationSeconds: 200},
+            {Title: "G Title", Artist: "G Artist", Album: "G Album", ReleaseYear: 2022, Genre: "Jazz", DurationSeconds: 220},
+            {Title: "H Title", Artist: "H Artist", Album: "H Album", ReleaseYear: 2021, Genre: "Rock", DurationSeconds: 200},
+            {Title: "I Title", Artist: "I Artist", Album: "I Album", ReleaseYear: 2022, Genre: "Jazz", DurationSeconds: 220},
         }
+
+        for _, song := range songs {
+            _, err = client.Exec(
+                `INSERT INTO songs (title, artist, album, release_year, genre, duration_seconds)
+                 VALUES ($1, $2, $3, $4, $5, $6)`,
+                song.Title, song.Artist, song.Album, song.ReleaseYear, song.Genre, song.DurationSeconds)
+
+            if err != nil {
+                log.Fatal(err)
+                fmt.Println("Error creating song")
+            }
+        }
+
+        fmt.Println("Song created successfully!")
     }
 
-    fmt.Println("Song created successfully!")
+    createReviewTableSQL := `CREATE TABLE IF NOT EXISTS reviews (
+        review_id SERIAL PRIMARY KEY,
+        user_id INT NOT NULL,
+        song_id INT NOT NULL,
+        data DATE NOT NULL,
+        reivew TEXT NOT NULL
+    );`
+
+    _, err = client.Exec(createReviewTableSQL)
+
+    if err != nil {
+        log.Fatal("Failed to create table: ", err)
+        return
+    } else {
+        log.Println("Table created successfully!")
+    }
 }
 
